f60aa4f13217d14652298c96e22f05f6abeef032
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-08-09 23:54:51 +0600

    TearDown method added

--- Eapp/UnitTest1.cs
+++ Eapp/UnitTest1.cs
@@ -5,7 +5,11 @@ using OpenQA.Selenium.Chrome;
 namespace Eapp
 {
     public class Tests
+
     {
+        //open the browser
+        //instantiation of driver
+        IWebDriver driver = new ChromeDriver();
         //hooks in nunit
         [SetUp]
         public void Setup()
@@ -16,13 +20,17 @@ namespace Eapp
         [Test]
         public void Test1()
         {
-            //open the browser
-            //instantiation of driver
-            IWebDriver driver = new ChromeDriver();
+            
             //navigate to site
             driver.Navigate().GoToUrl("http://eaapp.somee.com/Account/Login");
             driver.Manage().Window.Maximize();
             Assert.Pass();
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            driver.Quit();
         }
     }
 }
