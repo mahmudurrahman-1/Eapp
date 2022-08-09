d4be07e2bce1569265fd378d628bd43b1657e4a9
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-08-10 01:27:29 +0600

    modified with login and assertion

--- Eapp/UnitTest1.cs
+++ Eapp/UnitTest1.cs
@@ -1,3 +1,4 @@
+using Eapp.Login_step;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -6,25 +7,34 @@ namespace Eapp
 {
     public class Tests
 
-    {
+    {     
         //open the browser
         //instantiation of driver
-        IWebDriver driver = new ChromeDriver();
+       public static IWebDriver driver = null;
+       
         //hooks in nunit
         [SetUp]
         public void Setup()
         {
-         
+            if (driver == null)
+            {
+                driver = new ChromeDriver();
+            }
         }
 
         [Test]
         public void Test1()
         {
-            
+            //All instances
+            Login loginpage = new Login(driver);
+            //Identifiers 
+            //h2[normalize-space()='Login.']
+            By logintitle = By.XPath("//h2[normalize-space()='Login.']");
             //navigate to site
             driver.Navigate().GoToUrl("http://eaapp.somee.com/Account/Login");
             driver.Manage().Window.Maximize();
-            Assert.Pass();
+            loginpage.HandleLogin();
+            Assert.That(driver.FindElement(logintitle).Displayed, Is.True) ;
         }
 
         [TearDown]
