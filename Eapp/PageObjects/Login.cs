48e1478787ee61495b5585b2d811e55f6cc618a7
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-08-10 07:49:10 +0600

    login added

---
+++ Eapp/PageObjects/Login.cs
@@ -0,0 +1,40 @@
+﻿using Eapp.utils;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eapp.PageObjects
+{
+    
+    public class Login:Common
+    {
+        IWebDriver driver = null;
+        /************
+         * Identifiers
+         */
+        public Login(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        //Pom model
+       
+        By inputUsername = By.Id("UserName");
+        By inputUserPass = By.Id("Password");
+        By Loginbutton = By.CssSelector("input[value='Log in']");
+
+        public void HandleLogin(string name, string pass)
+        {
+            sendText(driver.FindElement(inputUsername), name);
+            sendText(driver.FindElement(inputUserPass), pass);
+           driver.FindElement(Loginbutton).Click();
+        }
+
+
+    }
+}
