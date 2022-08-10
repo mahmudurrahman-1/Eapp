b16d60fdd0d1380253127c00c9c780be8e215583
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-08-10 07:48:35 +0600

    driver folder added

---
+++ Eapp/Driver/BaseDriver.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eapp.Driver
+{
+    public class BaseDriver
+    {
+        public static IWebDriver driver = null;
+        public string baseurl = "http://eaapp.somee.com/Account/Login";
+
+        [SetUp]
+        public void startBrowser()
+        {
+            if(driver == null)
+            {
+                driver = new ChromeDriver();
+            }
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+        }
+     
+
+        [TearDown]
+        public void closeBrowser()
+        {
+           driver.Quit();
+        }
+    }
+}
